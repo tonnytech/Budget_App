dd3c0155c908a3957ab976a9e1c8cbcc2a042407
tonnytech <tonnytei4@gmail.com>
2023-10-20 08:08:20 +0300

    set up controllers and device

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,12 @@
 class CategoriesController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @current = current_user
+      @categories = Category.where(user_id: @current.id).includes(:spendings)
+    end
+
+    def new
+        @user = current_user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+    def index
+        @user = current_user
+        return unless user_signed_in?
+        redirect_to user_categories_path(user_id: @user.id)
+    end
 end
